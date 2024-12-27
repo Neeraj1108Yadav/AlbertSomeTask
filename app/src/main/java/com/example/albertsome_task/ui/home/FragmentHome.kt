e099f1bc3d17204e5dadff0d90ceeed185f76feb
Neeraj <neeraj@avantari.org>
2024-12-27 20:12:55 +0530

    list screen setup

--- app/src/main/java/com/example/albertsome_task/ui/home/FragmentHome.kt
+++ app/src/main/java/com/example/albertsome_task/ui/home/FragmentHome.kt
@@ -18,12 +18,11 @@ class FragmentHome : Fragment() {
         super.onCreate(savedInstanceState)
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 }
