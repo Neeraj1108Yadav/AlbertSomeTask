a971666de541471c32d81b73cbb8cce070577512
Neeraj <neeraj@avantari.org>
2024-12-28 16:21:56 +0530

    minor updates on ui

--- app/src/main/java/com/example/albertsome_task/util/ImageBindingAdapter.kt
+++ app/src/main/java/com/example/albertsome_task/util/ImageBindingAdapter.kt
@@ -3,12 +3,19 @@ package com.example.albertsome_task.util
 import androidx.appcompat.widget.AppCompatImageView
 import androidx.databinding.BindingAdapter
 import coil3.load
+import coil3.request.transformations
+import coil3.transform.CircleCropTransformation
 
-@BindingAdapter("imageUrl")
-fun bindImage(imageView: AppCompatImageView,url:String?){
+@BindingAdapter("imageUrl","isCircle", requireAll = false)
+fun bindImage(imageView: AppCompatImageView,url:String?,isCircle: Boolean?){
     url?.let {
         imageView.load(it) {
-
+            val width = imageView.getDrawable().intrinsicWidth
+            val height = imageView.getDrawable().intrinsicHeight
+            size(width,height)
+            if(isCircle == true){
+                transformations(CircleCropTransformation())
+            }
         }
     }
 }
