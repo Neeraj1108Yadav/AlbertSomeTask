6d7e0d05f2293e31cdc38b73c87086577dbd6e5c
Neeraj <neeraj@avantari.org>
2024-12-28 13:50:05 +0530

    list user design set

--- app/src/main/java/com/example/albertsome_task/ui/home/FragmentHome.kt
+++ app/src/main/java/com/example/albertsome_task/ui/home/FragmentHome.kt
@@ -7,19 +7,24 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.albertsome_task.R
+import com.example.albertsome_task.databinding.FragmentHomeBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class FragmentHome : Fragment() {
 
-    private val viewModel by viewModels<HomeViewModel>()
+    private lateinit var binding: FragmentHomeBinding
+    private val viewModel: HomeViewModel by viewModels<HomeViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding = FragmentHomeBinding.inflate(inflater,container,false)
+        binding.lifecycleOwner = viewLifecycleOwner
+        binding.viewmodel = viewModel
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
